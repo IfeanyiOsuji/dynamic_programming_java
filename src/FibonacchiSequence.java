792512abe4ec725edff32f53b4d0941156c71f6f
Ifeanyi Osuji <oindubuisi@gmail.com>
2023-10-24 15:57:18 +0100

    implemented tribonacci sequence using dynamic programming

--- src/FibonacchiSequence.java
+++ src/FibonacchiSequence.java
@@ -19,8 +19,29 @@ public class FibonacchiSequence {
     public static int fib(int n){
         return fib(n, new HashMap<>());
     }
+
+    public static int trib(int n){
+        return trib(n, new HashMap<>());
+    }
+    private static int trib(int n, Map<Integer, Integer>memo){
+        if(n==0 || n== 1){
+            return 0;
+        }
+        if(n==2)
+            return 1;
+        if(memo.containsKey(n)){
+            return memo.get(n);
+        }
+        int result = trib(n-1, memo) + trib(n-2, memo)+trib(n-3, memo);
+        memo.put(n, result);
+
+        return result;
+    }
+
     public static void main(String[] args) {
-        System.out.println(fib(1000));
+        //System.out.println(fib(1000));
+
+        System.out.println(trib(3));
         
     }
 }
